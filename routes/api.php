740411508ee0f01e076db1194dff372d102592f4
mahmood <mahmoodimtiaz.786@gmail.com>
2022-12-01 00:14:33 +0600

    Defined Routes for Post , Comment and Likes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +26,26 @@ Route::post('/login',[AuthController::class,'login']);
 
 //procted Routes
 Route::group(['middleware'=>['auth:sanctum']],function(){
+    
     //user
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/user',[AuthController::class,'user']);
+
+    //post
+    Route::get('/posts',[PostController::class,'index']);// all posts
+    Route::post('/posts',[PostController::class,'store']);// create post
+    Route::get('/posts/{id}',[PostController::class,'show']); // get single post
+    Route::put('/posts/{id}',[PostController::class,'update']); // update post
+    Route:: delete('/posts/{id}',[PostController::class,'destroy']);//delete post
+
+
+    //Comment
+    Route::get('/posts/{id}/comments',[CommentController::class,'index']);// all comments for a post
+    Route::post('/posts/{id}/comments',[CommentController::class,'store']);// create a comment on a post
+    Route::put('/comments/{id}',[CommentController::class,'update']); // update a comment
+    Route:: delete('/comments/{id}',[CommentController::class,'destroy']);//delete a comment
+
+    //Like
+    Route::post('/posts/{id}/likes',[LikeController::class,'likeorunlike']);// like or dislike a post
+
 });
